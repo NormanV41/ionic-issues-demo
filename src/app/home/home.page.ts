cf64f2e40a39075322058ba1182a821889a02766
Norman Vasquez <nrvasquez41@hotmail.com>
2021-11-06 11:16:47 -0600

    showing platform issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  platforms = '';
 
-  constructor() {}
-
+  constructor(platform: Platform) {
+    this.platforms = JSON.stringify(platform.platforms());
+  }
 }
