6e072832990088d7153a2e91bf6bcd4c30f98cee
Norman Vasquez <nrvasquez41@hotmail.com>
2021-11-07 11:51:23 -0600

    showing platform#isMobile issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { Platform } from '@ionic/angular';
 
 @Component({
@@ -8,8 +9,18 @@ import { Platform } from '@ionic/angular';
 })
 export class HomePage {
   platforms = '';
+  primaryPointerResult = '';
+  anyPointerResult = '';
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, @Inject(DOCUMENT) document: Document) {
+    const window = document.defaultView;
     this.platforms = JSON.stringify(platform.platforms());
+    this.primaryPointerResult = window.matchMedia('(pointer: coarse)').matches
+      ? 'The primary input mechanism includes a pointing device of limited accuracy, for example it is using touch as primary input device'
+      : 'The primary input mechanism does not includes a pointing device of limited accuracy' +
+        ', for example, touch is not the primary input device';
+    this.anyPointerResult = window.matchMedia('(any-pointer: coarse)').matches
+      ? 'At least one input mechanism includes a pointing device of limited accuracy, for example, touch is available'
+      : 'does not have at least one input mechanism includes a pointing device of limited accuracy, for example, touch is not available';
   }
 }
