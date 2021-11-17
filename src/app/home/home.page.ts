57c046e5e967d759807c4dd53ddaec025bf2c9e7
Norman Vasquez <nrvasquez41@hotmail.com>
2021-11-17 12:59:57 -0600

    showing datetime update issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public input = '';
+
+  public date = '2021-11-02';
 
   constructor() {}
 
+  public ngOnInit() {
+    setTimeout(() => {
+      this.date = '2021-11-05';
+      this.input = 'new value';
+    }, 10000);
+  }
 }
